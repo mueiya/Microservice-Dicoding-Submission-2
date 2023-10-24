1c0e82c7c2610d61fe08050f5a37aef3ab085ce0
mueiya <mueiya.whatever@gmail.com>
2023-10-24 15:38:46 +0700

    fix: the sh image tag

--- karsajobs-ui/build_push_image_karsajobs_ui.sh
+++ karsajobs-ui/build_push_image_karsajobs_ui.sh
@@ -3,10 +3,12 @@
 # 1. Perintah untuk membuat Docker image dari Dockerfile.
 # Baris ini menggunakan perintah docker build untuk membuat Docker image 
 # dari Dockerfile yang terdapat di direktori saat ini (.). 
-# Image ini kemudian diberi tag mueiya/karsajobs-ui:latest.
-docker build -t mueiya/karsajobs-ui:latest .
+# Image ini kemudian diberi tag karsajobs-ui:latest.
+docker build -t karsajobs-ui:latest .
 
 # 2. Login ke GitHub Packages
+# ubah tag dan nama, sesuai penamaan github package
+docker tag karsajobs-ui:latest ghcr.io/mueiya/karsajobs-ui:latest
 # karena (Personal Access Token) bersifat rahasia, 
 # pastikan untuk menjalankan code dibawah untuk menyimpan PAT dalam ENV
 # export GITHUB_PAT=personal_access_token
